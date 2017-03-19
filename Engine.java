f825410946a4f8bcb14b6d59cc3ec75008d90e8b
GShavoshvili <g30.l3g3nd@gmail.com>
2017-03-19 13:33:02 +0400

    Add files via upload

---
+++ Engine.java
@@ -0,0 +1,74 @@
+package snake;
+
+import java.awt.Dimension;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/*
+/ The Engine class holds reusable static methods
+ */
+public class Engine {
+
+    static final JFrame createFrame(JPanel panel, int width, int height) {
+        JFrame frame = new JFrame();
+        Dimension size = new Dimension(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setFocusable(true);
+        panel.setPreferredSize(size);
+
+        frame.add(panel);
+
+        frame.setResizable(false);
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+        return frame;
+    }
+
+    static final Clip createClip(String name, Class cl) {
+
+        Clip clip = null;
+        try (AudioInputStream stream = AudioSystem.getAudioInputStream(cl.getResource(name))) {
+
+            clip = AudioSystem.getClip();
+            clip.open(stream);
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return clip;
+
+    }
+
+    static final void resetClip(Clip c) {
+        c.setFramePosition(0);
+        c.stop();
+    }
+
+    static final void resetClip(Clip c[]) {
+        for (Clip x : c) {
+            x.setFramePosition(0);
+            x.stop();
+        }
+
+    }
+
+    static final void restartClip(Clip c) {
+        c.setFramePosition(0);
+        c.start();
+    }
+
+}
---
+++ Sounds.java
@@ -0,0 +1,56 @@
+package snake;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.Clip;
+
+
+/*
+/ The Sound class handles sounds 
+ */
+public class Sounds {
+
+    Clip eat;
+    Clip snake1;
+    Clip snake2;
+    Clip snake3;
+    Clip snake4;
+    Clip snake5;
+    Clip snake6;
+    Clip snake7;
+    Clip snake8;
+    Clip lostSounds[] = new Clip[8];
+
+    Sounds(Class game) {
+
+        eat = Engine.createClip("Eat.wav", game);
+        snake1 = Engine.createClip("snake1.wav", game);
+        snake2 = Engine.createClip("snake2.wav", game);
+        snake3 = Engine.createClip("snake3.wav", game);
+        snake4 = Engine.createClip("snake4.wav", game);
+        snake5 = Engine.createClip("snake5.wav", game);
+        snake6 = Engine.createClip("snake6.wav", game);
+        snake7 = Engine.createClip("snake7.wav", game);
+        snake8 = Engine.createClip("snake8.wav", game);
+
+        lostSounds[0] = snake1;
+        lostSounds[1] = snake2;
+        lostSounds[2] = snake3;
+        lostSounds[3] = snake4;
+        lostSounds[4] = snake5;
+        lostSounds[5] = snake6;
+        lostSounds[6] = snake7;
+        lostSounds[7] = snake8;
+
+    }
+
+    void resetLost() {
+        Engine.resetClip(lostSounds);
+
+    }
+
+    void restartEat() {
+        Engine.restartClip(eat);
+    }
+
+}
